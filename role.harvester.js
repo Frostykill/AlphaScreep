c88cd93e3fe9c89c622dfa56e4bf329ca2670713
frostykill <jonathan.montgo@gmail.com>
2017-04-27 20:53:51 -0500

    Minor changes

--- role.harvester.js
+++ role.harvester.js
@@ -1,41 +1,43 @@
 var roleHarvester = {
-
-    /** @param {Creep} creep **/
-    run: function(creep) {
-        if(creep.carry.energy < creep.carryCapacity) {
-            var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
-            }
+  /** @param {Creep} creep **/
+  run: function(creep) {
+    if(creep.carry.energy < creep.carryCapacity) {
+      var sources = creep.room.find(FIND_SOURCES);
+      for (let source in sources){
+        let free_pos ==
+        if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
         }
-        else {
-            var targets = creep.room.find(FIND_STRUCTURES, {
-                filter: (structure) => {
-                    return (structure.structureType == STRUCTURE_EXTENSION ||
-                        structure.structureType == STRUCTURE_SPAWN ||
-                        structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
-                }
-            });
-            if(targets.length > 0) {
-                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
-                }
+      }
+      else {
+        var targets = creep.room.find(FIND_STRUCTURES, {
+          filter: (structure) => {
+            return (structure.structureType == STRUCTURE_EXTENSION ||
+              structure.structureType == STRUCTURE_SPAWN ||
+              structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
             }
+          });
+          if(targets.length > 0) {
+            if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+              creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+            }
+          }
         }
-    },
+      },
 
-    /** Get's all creeps of role harvester **/
-    getHarvesters: ()=>{
-      return _.filter(Game.creeps, {memory: {role: "harvester"}});
-    },
+      /** Get's all creeps of role harvester **/
+      getHarvesters: ()=>{
+        return _.filter(Game.creeps, {memory: {role: "harvester"}});
+      },
 
-    /** Spawns a harvester @param {Spawn} spawn **/
-    spawnHarvester: (spawn)=>{
-      return spawn.createCreep(
-        [CARRY, MOVE, WORK],
-        undefined,
-        {role: "harvester"})
+      /** Spawns a harvester @param {Spawn} spawn **/
+      spawnHarvester: (spawn)=>{
+        return spawn.createCreep(
+          [CARRY, MOVE, WORK],
+          undefined,
+          {role: "harvester"})
+        }
+      };
     }
-};
 
 module.exports = roleHarvester;
